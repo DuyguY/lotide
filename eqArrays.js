7b460f56840653f49dc9c218239133ca3574b76a
DuyguY <duygu.yldrm4820@gmail.com>
2021-11-29 15:07:14 -0500

    adding test files

--- eqArrays.js
+++ eqArrays.js
@@ -21,8 +21,4 @@ const eqArrays = function (input1, input2){
 
 };
 
-eqArrays([1,2,3], [1,2,3]);
-eqArrays([1,2,3], [3,2,1]);
-eqArrays(["1","2","3"], ["1","2","3"]);
-eqArrays(["1","2","3"], ["1","2",3]);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
