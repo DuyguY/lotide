2cd17d5c002bb34fd67f9e008eb5cadfb75c3c8d
DuyguY <duygu.yldrm4820@gmail.com>
2021-11-29 17:06:33 -0500

    adding new test with mocha and chain

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,17 @@
-const assertEqual = require('../assertEqual');
+/* const assertEqual = require('../assertEqual');
 const head = require('../head');
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([5,6,7]), 9);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hi");
+
 assertEqual(head([5]), 9);
-assertEqual(head([]), "Hi"); 
+assertEqual(head([]), "Hi");  */
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
